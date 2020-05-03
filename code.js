65f466c54b8df0272b9a54f3b75a9d05291e473c
Toby Okeke <toby.okeke@gmail.com>
2020-05-03 18:56:54 +0100

    initial commit

---
+++ code.js
@@ -0,0 +1,34 @@
+// This plugin will open a modal to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser enviroment (see documentation).
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
+figma.ui.postMessage({
+    type: "text",
+    number: figma.currentPage.selection.length
+});
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+figma.ui.onmessage = msg => {
+    // One way of distinguishing between different types of messages sent from
+    // your HTML page is to use an object with a "type" property like this.
+    msg = JSON.parse(msg);
+    console.log(msg.type);
+    if (msg.type === 'names' || msg.type === 'phones' || msg.type === 'emails') {
+        figma.loadFontAsync({ family: "Roboto", style: "Regular" }).then(() => {
+            let count = 0;
+            figma.currentPage.selection.forEach((item) => {
+                console.log(msg.data[count]);
+                item.deleteCharacters(0, item.characters.length);
+                item.insertCharacters(0, msg.data[count]);
+                count++;
+            });
+        });
+    }
+    // Make sure to close the plugin when you're done. Otherwise the plugin will
+    // keep running, which shows the cancel button at the bottom of the screen.
+    // figma.closePlugin();
+};
